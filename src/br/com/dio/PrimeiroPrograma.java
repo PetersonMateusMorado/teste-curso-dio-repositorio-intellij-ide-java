0483cd922e06cc8dc799cbfc4d671572e3db6900
Peterson <pm1733@gmail.com>
2022-01-28 14:49:44 -0300

    First commit classe gato

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package br.com.dio;
+
+import br.com.dio.gato.Gato;
+
+public class PrimeiroPrograma {
+
+        public static void main(String[] args){
+
+                Gato gato = new Gato();
+
+                System.out.println(gato);
+
+
+
+
+
+
+        }
+
+}
+
+
+
+
